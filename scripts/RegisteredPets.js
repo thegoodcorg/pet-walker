ab43a4a9b243dff6684ba06f3da469958c3e3336
Jeremy Goodman <jeremygoodman52@gmail.com>
2022-11-28 11:45:40 -0600

    added walker and pet event clickers

--- scripts/RegisteredPets.js
+++ scripts/RegisteredPets.js
@@ -1,12 +1,28 @@
 import { getPets } from "./database.js"
 
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+
+        if(itemClicked.id.startsWith("pet")) {
+            const [,petId] = itemClicked.id.split("--")
+            for (const pet of pets) {
+                if (pet.id === parseInt(petId)) {
+                    window.alert(`${pet.name} barks at you!`)
+                }
+            }
+        }
+    }
+    )
+
 const pets = getPets()
 
 export const RegisteredPets = () => {
     let petHTML = "<ul>"
 
     for (const pet of pets) {
-        petHTML += `<li>${pet.name}</li>`
+        petHTML += `<li id="pet--${pet.id}">${pet.name}</li>`
     }
 
     petHTML += "</ul>"
